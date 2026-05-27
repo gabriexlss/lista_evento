2a3ceb690c28a8a45a1b5b1056df5b95b8ca3696
gabriexlss <gabrielsasukeclash@gmail.com>
2026-05-27 12:20:29 -0300

    Tela de Consumo Parcialmente Concluida

--- client/src/pages/Principal/pages/Consumo/index.jsx
+++ client/src/pages/Principal/pages/Consumo/index.jsx
@@ -1,8 +1,63 @@
-import React from 'react'
-
+import { Calendar, MapPin, Cake, Users, Plus } from 'lucide-react'
+import "./Consumo.css"
 const Consumo = () => {
   return (
-    <div>Consumo</div>
+    <div className="Consumo">
+      <h1 className="ConsumoTitle">Aniversario Vinicius</h1>
+      <div className="ConsumoHorarioeLocal">
+        <Calendar size={20} />
+        <p>15:00h</p>
+        <p> - </p>
+        <MapPin size={20} />
+        <p>Habibs</p>
+      </div>
+      <div className="ConsumoAniversariante">
+        <Cake size={16} />
+        <p>Aniversariante do dia: Carlos Vinicius</p>
+      </div>
+      <div className="ConsumoTotal">
+        <h4>Total da Mesa</h4>
+        <span className="ConsumoTotalValorContainer">
+          <div className="ConsumoTotalSifra">R$</div>
+          <div className="ConsumoTotalValor">485</div>
+          <div className="ConsumoTotalCentavos">,50</div>
+        </span>
+        <span className="ConsumoTotalPorcentagem">
+          <div className="ConsumoTotalPorcentagemInfo">
+            <p>Pago</p>
+            <p>45%</p>
+          </div>
+          <div className="ConsumoTotalPorcentagemBarra">
+            <div className="ConsumoTotalPorcentagemBarraPreenchida" style={{ width: '45%' }}></div>
+          </div>
+        </span>
+      </div>
+      <div className="ConsumoPessoas">
+        <span>
+          <Users size={28} />
+        </span>
+        <p>10</p>
+        <p>Pessoas</p>
+      </div>
+      <div className="ConsumoPessoas">
+        <span>
+          <Users size={28} />
+        </span>
+        <p>10</p>
+        <p>Pessoas</p>
+      </div>
+      <div className="ConsumoParticipantes">
+        <h2>Participantes</h2>
+      </div>
+      <div className="ConsumoLancar">
+        <button>
+          <div className="ConsumoLancarIcon">
+            <Plus size={20} />
+          </div>
+          <p>Registrar Novo Consumo</p>
+        </button>
+      </div>
+    </div>
   )
 }
 
